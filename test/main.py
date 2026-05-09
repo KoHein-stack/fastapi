05da4838c120adca99b9405d38abbeb0720cfcda
Han Naung Soe <hannaung@blue-stone.net>
2026-05-10 00:27:56 +0630

    Add in-memory post endpoints

--- test/main.py
+++ test/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from random import randrange
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -11,7 +11,10 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}]
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite foods", "content": "I like pizza", "id": 2}
+
+            ]
     
 @app.get("/")
 async def root():
@@ -19,12 +22,27 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": "Here are your posts!"}
+    return {"data": my_posts}
 
 @app.post("/create_post")
 async def create_post(post: Post = Body(...)):
     print(post)
     print(post.dict())
-    return {"new_post": f"Title: {post.title} Content: {post.content} Published: {post.published} Rating: {post.rating}",
-             "message": "Post created successfully!", "post": post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"new_post": f"Title: {post_dict['title']} Content: {post_dict['content']} Published: {post_dict['published']} Rating: {post_dict['rating']}",
+             "message": "Post created successfully!", "post": post_dict}
+
+@app.get("/posts/{id}")
+async def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail": post}
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+    return None
+
 
